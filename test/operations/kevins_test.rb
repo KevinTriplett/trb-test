71e96cb1eea4e82c40804c77eca92d0d56c96d20
Kevin Triplett <5772+KevinTriplett@users.noreply.github.com>
2022-10-25 11:43:53 +0200

    testing 1

--- test/operations/kevins_test.rb
+++ test/operations/kevins_test.rb
@@ -2,15 +2,17 @@ require "test_helper"
 
 class KevinsTest < Minitest::Spec
   class Create < Trailblazer::Operation
-    step :model
+    step :model {
+      In() => [:title]
+    }
 
-    def model(ctx, params:, **)
-      raise params.inspect
+    def model(ctx, title:, **)
+      raise title.inspect
     end
   end
 
   # Run without debugging:
   # result = Create.(params: {title: "Suggestion for democracy"})
-  result = Create.wtf?(params: {title: "Suggestion for democracy"})
+  result = Create.wtf?(title: "Suggestion for democracy", params: {name: "Joe Kid"})
 
 end
