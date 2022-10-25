335794e0bcd10faa2cfe4408788c46aca917db24
Kevin Triplett <5772+KevinTriplett@users.noreply.github.com>
2022-10-25 11:57:18 +0200

    test 2

--- test/operations/kevins_test.rb
+++ test/operations/kevins_test.rb
@@ -4,10 +4,17 @@ class KevinsTest < Minitest::Spec
   class Create < Trailblazer::Operation
     step :model,
       In() => [:title]
+      Out() => [:name]
+    step :show_name
 
     def model(ctx, title:, **)
       puts title.inspect
       puts ctx[:params].inspect
+      ctx[:name] = title
+    end
+
+    def show_name(ctx, **)
+      puts ctx[:name]
       true
     end
   end
@@ -15,5 +22,6 @@ class KevinsTest < Minitest::Spec
   # Run without debugging:
   # result = Create.(params: {title: "Suggestion for democracy"})
   result = Create.wtf?(title: "Suggestion for democracy", params: {name: "Joe Kid"})
+
 
 end
